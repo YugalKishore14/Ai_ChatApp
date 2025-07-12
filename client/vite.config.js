f24fec5b6be243cd99b1b68cc060d3debcbaf100
YugalKishore14 <yugaldhiman14@gmail.com>
2025-07-12 14:47:18 +0530

    Add initial HTML and SVG assets for YUG-AI project

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Use relative paths
+  base: 'http://34.42.233.32:5000/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
